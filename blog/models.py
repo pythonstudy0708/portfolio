4506bc63bc9c7bd022cbcf1171e08026045e88c0
Tuan Nguyen <python.study.0708@gmail.com>
2018-09-20 15:10:54 +0700

    update blog and jobs view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# Create a blog models
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+    tagline = models.TextField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the blog app to the settings
+
+#create sa migration
+
+#migrate
+
+#add to the admin
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,4 @@
-"""porfolio URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name = "home"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
