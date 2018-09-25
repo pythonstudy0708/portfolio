03f8950c48613d65cd4c50505952b1c4c548a3b7
Tuan Nguyen <python.study.0708@gmail.com>
2018-09-25 13:55:21 +0700

    Blog functionlity and polish

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,12 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
 # Add the blog app to the settings
 
 #create sa migration
